4cdf78338565f9a7af6037d66ac33927213572e3
Rangerson Clemente <rangerson14@outlook.com>
2022-10-31 00:28:44 -0300

    Classe Principal
    Código da classe principal do projeto Material Escolar

---
+++ Material_Escolar/Principal.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Material_Escolar
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            Apontador A = new Apontador();
+            Lapis L = new Lapis();
+            Caneta C = new Caneta();
+            Borracha B = new Borracha("Mercur", "Verde", 7, "Lozango");
+            Caderno E = new Caderno();
+
+
+            C.SetCor("Verde");
+            C.SetTamanho(14);
+            C.SetPeso(30);
+            C.SetTampada(true);
+            C.SetPercentCarga(87);
+            C.SetMarca("Bic");
+            C.SetMaterial("Acrílico");
+            C.SetPonta(3.7);
+            C.SettipoPonta("Esferica");
+            C.Setcaida(false);
+            C.Setestourada(true);
+
+            A.Setcego(false);
+            A.Setguardado(false);
+            A.Setenferrujado(false);
+            A.Setcor("Amarelo");
+            A.Setmarca("ApontME");
+            A.Setforma("Retangulo");
+            A.Settamanho(7);
+
+            E.Setquantfolha(300);
+            E.Setquantmateria(10);
+            E.Setmarca(" Turma do Jandaia");
+            E.Settipocapa(" Brochura");
+            E.Setpauta(true);
+            E.Setespiral(true);
+            E.aberto = false;
+
+            Console.WriteLine("\n********Caderno********\n");
+            E.Arrancarfolha();
+            E.Escrevernocaderno();
+            E.Pintar();
+
+            L.Setcor("Preto");
+            L.Settipomaterial("Madeira");
+            L.Setapontado(true);
+            L.SetpercentGrafite(6);
+
+            Console.WriteLine("\n********Apontador********\n");
+            A.Apontar();
+
+            Console.WriteLine("\n********Lápis********\n");
+            L.Pintar();
+            L.Escrever();
+
+            Console.WriteLine("\n********Borracha********\n");
+            //B.limpar();
+            //B.Apagar();
+            //B.Status();
+
+        }
+    }
+}
