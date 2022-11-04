c6c4ae0e317dc0647409e3ad09cc387e805cf2dc
Rangerson Clemente <rangerson14@outlook.com>
2022-11-04 17:10:10 -0300

    arrumar

---
+++ SistemaEleitoral/SistemaEleitoral/BLL/CandidatoBLL.cs
@@ -0,0 +1,42 @@
+﻿using DAL;
+using Models;
+using System;
+
+namespace BLL
+{
+    public class CandidatoBLL
+    {
+        public void Inserir(Candidato _candidato)
+        {
+            if (_candidato.Getnome().Length <= 2)
+            {
+                throw new Exception("Nome do eleitor deve ter mais de 2 caracteres.");
+            }
+
+            CandidatoDAL candidatoDAL = new CandidatoDAL();
+            candidatoDAL.Inserir(_candidato);
+        }
+
+        public void Excluir(Candidato _candidato)
+        {
+            if (_candidato.Getnome().Length <= 2)
+            {
+                throw new Exception("Nome do eleitor deve ter mais de 2 caracteres.");
+            }
+            CandidatoDAL candidatoDAL = new CandidatoDAL();
+            candidatoDAL.Excluir(_candidato);
+        }
+
+        public void Alterar(Candidato _candidato)
+        {
+            CandidatoDAL candidatoDAL = new CandidatoDAL();
+            candidatoDAL.Alterar(_candidato);
+        }
+
+        public List<Eleitor> Buscar(string _titulo)
+        {
+            EleitorDAL eleitorDAL = new EleitorDAL();
+            return eleitorDAL.Buscar(_titulo);
+        }
+    }
+}
---
+++ SistemaEleitoral/SistemaEleitoral/BLL/EleitorBLL.cs
@@ -0,0 +1,41 @@
+﻿using Models;
+using DAL;
+
+namespace BLL
+{
+    public class EleitorBLL
+    {
+        public void Inserir(Eleitor _eleitor)
+        {
+            if(_eleitor.Getnome().Length <= 2)
+            {
+                throw new Exception("Nome do eleitor deve ter mais de 2 caracteres.");
+            }
+
+            EleitorDAL eleitorDAL = new EleitorDAL();
+            eleitorDAL.Inserir(_eleitor);
+        }
+
+        public void Excuir(Eleitor _eleitor)
+        {
+            if (_eleitor.Getnome().Length <= 2)
+            {
+                throw new Exception("Nome do eleitor deve ter mais de 2 caracteres.");
+            }
+            EleitorDAL eleitorDAL = new EleitorDAL();
+            eleitorDAL.Excluir(_eleitor);
+        }
+
+        public void Alterar(Eleitor _eleitor)
+        {
+            EleitorDAL eleitorDAL=new EleitorDAL();
+            eleitorDAL.Alterar(_eleitor);
+        }
+ 
+        public List<Eleitor> Buscar(string _titulo)
+        {
+            EleitorDAL eleitorDAL = new EleitorDAL();
+            return eleitorDAL.Buscar(_titulo);
+        }
+    }
+}
